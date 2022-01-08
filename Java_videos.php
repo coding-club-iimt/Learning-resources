3e9f5b7d43a3fbe6a3876a041db75ff8a1c53b0f
coding-club-iimt <95184981+coding-club-iimt@users.noreply.github.com>
2022-01-08 16:25:36 +0530

    Add files via upload

---
+++ Java_videos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>   
+    <head>
+        <meta charset="utf - 8">    
+        <title>Java Videos</title>
+        <link rel="stylesheet" href="style.css">
+    </head> 
+    <body>
+
+        
+      
+
+
+        <ul class="tilesWrap">
+            <?php  
+
+
+            $jsonData = file_get_contents("java-videos.json");
+            $data = json_decode($jsonData , true);
+            $count = 1;
+
+            foreach($data as $key => $value){
+    
+  
+            
+            
+            ?>
+            <a href="<?php echo $value['link']; ?>">
+            <li>
+                <h2><?php echo $count++;?></h2>
+                <h3><?php echo $value['title']; ?></h3>
+                <p>
+                    <?php
+                     echo $value['description'];
+                    ?>
+                </p>
+                <button> Read more</button>
+            </li> </a>
+            <?php  } ?> 
+           
+          
+        </ul>
+
+
+
+    </body>   
+    
+
+</html>
